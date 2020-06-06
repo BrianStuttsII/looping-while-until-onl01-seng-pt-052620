d1593de5277c8b9f273dfc3ede4d60d2713dc993
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-06 20:08:44 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -3,11 +3,9 @@ def using_until
   levitation_ force = 6
   
   until levitation_force == 10 
-  
-  levitation_force += 1 
  
   puts "Wingardium Leviosa"
-  
+  levitation_force += 1 
   end
 end
 
