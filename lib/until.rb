0343e92327275ce57707f42c0dc4aa0fc64709e5
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-06 20:20:23 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -13,8 +13,8 @@ def using_until
   end
 end
 
-counter = 0
-until counter == 20
-  puts "The current number is less than 20."
-  counter += 1
-end
+# counter = 0
+# until counter == 20
+  # puts "The current number is less than 20."
+ # counter += 1
+# end
